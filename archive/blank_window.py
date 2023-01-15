d9770e94108364838c23b07c8b1147726eeefd3b
daniel <danvargg@gmail.com>
2023-01-15 18:59:56 -0500

    more updates

--- archive/blank_window.py
+++ archive/blank_window.py
@@ -11,6 +11,7 @@ pg.display.set_caption('Blank Window')
 clock = pg.time.Clock()
 
 ship_surface = pg.image.load('graphics/ship.png').convert_alpha()  # .png does not run fast
+# ship_reversed_surface = pg.transform.flip(ship_surface, False, True)  # flip horizontally, vertically
 ship_rectangle = ship_surface.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
 laser_surface = pg.image.load('graphics/laser.png').convert_alpha()
@@ -26,6 +27,8 @@ font = pg.font.Font('fonts/subatomic.ttf', 30)
 text_surface = font.render('Asteroid Shooter', True, (255, 255, 255))
 text_rectangle = text_surface.get_rect(midbottom=(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
 
+test_rectangle = pg.Rect(100, 200, 400, 500)  # left, top, width, height
+
 while True:
     # 1. player input -> events
     for event in pg.event.get():
@@ -36,15 +39,20 @@ while True:
     # 1.5. frame rate limit
     clock.tick(120)
 
-    # ship_rectangle.center = pg.mouse.get_pos()
+    ship_rectangle.center = pg.mouse.get_pos()
     laser_rectangle.y -= 10
 
     #2. update game state
     # display_surface.fill((0, 0, 0))
     display_surface.blit(background_surface, (0, 0))
-    display_surface.blit(ship_surface, ship_rectangle)
+    
     display_surface.blit(text_surface, text_rectangle)
     display_surface.blit(laser_surface, laser_rectangle)
+
+    pg.draw.rect(display_surface, 'white', text_rectangle.inflate(50, 25), width=10, border_radius=10)
+    # pg.draw.lines(display_surface, 'white', True, [(0, 0), (200, 0), (300, 100)], width=1)
+
+    display_surface.blit(ship_surface, ship_rectangle)
 
     #3. update display surface / show frame to player
     pg.display.update()
