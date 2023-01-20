09d83bf7829625b6765bd1e0df8faa02becc893f
daniel <danvargg@gmail.com>
2023-01-19 22:32:03 -0500

    adds more reqs, initial commit for game install

---
+++ install_game.py
@@ -0,0 +1,20 @@
+"""Converts the game into an .exe file."""
+import sys
+import pygame
+import cx_Freeze
+
+base = None
+
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+executables = [cx_Freeze.Executable("src/start_game.py", base=base)]
+
+cx_Freeze.setup(
+    name="Asteroid Shooter",
+    options={"build_exe": {"packages": ["pygame"], "include_files": ["src/graphics.png", "src/fonts.jpg"]}},
+    executables=executables
+)
+
+# TODO: update reqs.txt: pygame, cs_Freeze only?
+# python install_game.py build
